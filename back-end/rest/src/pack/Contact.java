d9206092c393366d8663c2730cc4e17debac2072
hamza <hz.mouddene@gmail.com>
2021-04-22 04:30:58 +0200

    #

--- back-end/rest/src/pack/Contact.java
+++ back-end/rest/src/pack/Contact.java
@@ -1,13 +1,9 @@
 package pack;
 
-import java.util.Collection;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Contact {
--- back-end/rest/src/pack/Facade.java
+++ back-end/rest/src/pack/Facade.java
@@ -1,7 +1,5 @@
 package pack;
 
-import java.util.Collection;
-
 import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,8 +7,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
 
 @Singleton
 @Path("/")
