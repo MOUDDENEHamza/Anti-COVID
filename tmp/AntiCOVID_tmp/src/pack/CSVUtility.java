b7a072c5b08bd1778c0a3390b3ded529c2e3a6d3
hamza <hz.mouddene@gmail.com>
2021-05-06 09:29:43 +0200

    #

---
+++ tmp/AntiCOVID_tmp/src/pack/CSVUtility.java
@@ -0,0 +1,73 @@
+package pack;
+
+import java.util.*;
+import java.io.*;
+
+public class CSVUtility {
+	
+	/**
+	 * Parse a CSV file and extract covid data stored on owid-covid-data.csv
+	 * @throws FileNotFoundException 
+	 * @throws CsvException 
+	 * @throws IOException 
+	 */
+	public static List<List<String>> parseCSVCovidFile() throws FileNotFoundException, IOException {
+		String fileName = "/home/hamza/Desktop/Anti-COVID/back-end/AntiCOVID/data/owid-covid-data.csv";
+		List<List<String>> records = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		        String[] values = line.split(",");
+		        List<String> item = new ArrayList<>();
+		        
+		        if ((values[2]).equals("France")) {
+		        	item.add(values[3]); // date        0
+		        	item.add(values[4]); // total_cases 1
+		        	item.add(values[5]); // new_cases   2
+		        	item.add(values[6]); // recovered   3
+		        	item.add(values[8]); // death       4
+		        	records.add(item);
+		        }
+		    }
+		}
+		return records;
+	}
+	
+	/**
+	 * Parse a CSV file and extract vaccin center data stored on centres-vaccination.csv
+	 * @throws FileNotFoundException 
+	 * @throws CsvException 
+	 * @throws IOException 
+	 */
+	public static List<List<String>> parseCSVVaccinFile() throws FileNotFoundException, IOException {
+		String fileName = "/home/hamza/Desktop/Anti-COVID/back-end/AntiCOVID/data/centres-vaccination.csv";
+		List<List<String>> records = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		        String[] values = line.split(";");
+		        List<String> item = new ArrayList<>();
+	        	item.add(values[1]);  		// nom				0
+	        	item.add(values[5]);  		// adr_num			1
+	        	item.add(values[6]);  		// adr_voie			2
+	        	item.add(values[7]);  		// com_cp 			3
+	        	item.add(values[9]);  		// com_nom 			4
+	        	item.add(values[14]); 		// structure_rais	5
+	        	if (values.length > 34) {
+	        		item.add(values[34]); 	// rdv_site_web		6
+	        	} else {
+	        		item.add("");
+	        	}
+	        	if (values.length > 35) {
+	        		item.add(values[35]);	// rdv_tel			7
+	        	} else {
+	        		item.add("");
+	        	}
+	        	
+	        	records.add(item);
+		    }
+		}
+		return records;
+	}
+
+}
---
+++ tmp/AntiCOVID_tmp/src/pack/EmailUtility.java
@@ -0,0 +1,54 @@
+package pack;
+
+import java.util.Date;
+import java.util.Properties;
+ 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+/**
+ * A utility class for sending e-mail messages
+ * @author Hamza Mouddene
+ *
+ */
+public class EmailUtility {
+	
+    public static void sendEmail(String toAddress, String subject,
+    		String message) throws AddressException, MessagingException {
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("anticovid.service.client@gmail.com", "anticovid@2021");
+            }
+        };
+ 
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress("anticovid.service.client@gmail.com"));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        msg.setText(message);
+        
+        // sends the e-mail
+        Transport.send(msg);
+    }
+}
