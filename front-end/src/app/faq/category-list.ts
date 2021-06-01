332855b5df2770bddb87499f0e208910f287046c
axelle <axelle.pagnier@gmail.com>
2021-06-01 18:25:56 +0200

    faq

---
+++ front-end/src/app/faq/category-list.ts
@@ -0,0 +1,26 @@
+
+export interface Category {
+    name: string;
+    infos: any;
+}
+
+export const CATEGORY: Category[] = [
+    {
+        name: 'Fonctionnement du site',
+        infos: 'blablabla sur le Fonctionnement du site',
+    },
+    {
+        name: 'Informations covid',
+        infos: 'blabla sur les Informations covid',
+    },
+    {
+        name: 'Informations vaccins',
+        infos: 'blabla sur les Informations vaccins',
+
+    },
+    {
+        name: 'Autre',
+        infos: 'voir forum et Contact',
+
+    }
+];
